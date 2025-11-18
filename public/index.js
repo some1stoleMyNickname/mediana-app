bf40bae852cef5b140f8b52f12ee8e044bb0c57e
Ales Pavli <ales.pavli@gmail.com>
2025-11-18 22:37:02 +0100

    thirdV

---
+++ public/index.js
@@ -0,0 +1,261 @@
+//
+const randNumbers = (min, max, times) => {
+  const oneRandomNumber = []
+
+  for (let i = 0; i < times; i++) {
+      oneRandomNumber.push(Math.floor(Math.random() * (1000 - 1) + 1));
+  }
+  return oneRandomNumber;
+};
+
+//funkcije ki se sprožijo ob nalaganju okna
+window.onload = function() {
+  generateRandomNumbers();
+  pStevila();
+};
+
+//generiraj 5 random števil
+document.getElementById("generateRandomNumbers").onclick = function(){
+  generateRandomNumbers();
+};
+
+function generateRandomNumbers() {
+  const stevilke = randNumbers(1, 1000, 5);
+  const vseStevilke = document.getElementById("stevilke");
+  vseStevilke.innerHTML = stevilke.join(", ");
+};
+
+//izbris enega naključno izbranega števila
+document.getElementById("izbris1").onclick = function(){
+  izbris();
+};
+
+function izbris() {
+  const vsaSt = document.getElementById("stevilke");
+  const prikSt = vsaSt.innerHTML;
+  const stevilke = prikSt.split(", ").map(Number);
+
+//random določi število
+
+  if (stevilke.length <= 1) {
+    alert("Dodajte število/a!");
+  } else {
+  const rIzbris = Math.floor(Math.random() * stevilke.length);
+  stevilke.splice(rIzbris, 1);
+
+  vsaSt.innerHTML = stevilke.join(", ");
+  }
+};
+
+//eno naključno generirano število
+document.getElementById("enoStNakljucno").onclick = function(){
+  eno_st();
+}
+
+function eno_st() {
+  const vseStevilke = document.getElementById("stevilke");
+  const pStevila = vseStevilke.innerHTML;
+  const stevilke = randNumbers(1, 1000, 1);
+  const novaStevila = stevilke.join(", ");
+  if (pStevila) {
+    vseStevilke.innerHTML = pStevila + ", " + novaStevila;
+  } else {
+    vseStevilke.innerHTML = novaStevila;
+  }
+};
+
+//izbris vseh
+document.getElementById("izbrisVse").onclick = function(){
+  izbrisV();
+};
+
+function izbrisV() {
+  const abc = document.getElementById("stevilke");
+  if (abc.innerHTML == []) {
+    alert("Ni podanih števil!");
+  } else {
+    abc.innerHTML = [];
+  };
+};
+
+//dodaj število
+function formSubmit() {
+  const form = document.forms["selectNumberSlider"];
+  const inputValue = form["amountInput"].value;
+
+  if (inputValue >= 1 && inputValue <= 1000) {
+    const addNumber = document.getElementById("stevilke");
+
+    if (addNumber.innerHTML) {
+      const stevilke = addNumber.innerHTML.split(", ").map(Number);
+      stevilke.push(inputValue);
+      addNumber.innerHTML = stevilke.join(", ");
+    } else {
+      const stevilke = [inputValue];
+      addNumber.innerHTML = stevilke.join(", ");
+    }
+  } else {
+    alert("Vnesite veljavno število med 1 in 1000.");
+  }
+}
+
+function calculateXOR(stevilke) {
+  let xorValue = 0;
+
+  stevilke.forEach(function (number) {
+    xorValue ^= number;
+  });
+
+  return xorValue;
+}
+
+//podatki za tabelo
+function pStevila() {
+  axios.get("http://localhost:5500/api/mediana/get")
+    .then(function(response) {
+      const data = response.data;
+      const medianaValues = data.mediana;
+      const averageValues = data.average;
+      const xorValues = [];
+
+      const tbody = document.querySelector("#TabelaSt tbody");
+
+      // Izračun XOR med stevilo in avgValue na vsakem mestu
+      for (let i = 0; i < medianaValues.length && i < averageValues.length; i++) {
+        const xorValue = calculateXOR([medianaValues[i], averageValues[i]]);
+        xorValues.push(xorValue);
+      }
+
+      function addLeadingZeros(value, length) {
+        const binaryString = value.toString(2);
+        const zerosToAdd = length - binaryString.length;
+        return "0".repeat(zerosToAdd) + binaryString;
+      }
+
+      // Izpis vrednosti v ustrezne celice
+      for (let i = 0; i < medianaValues.length; i++) {
+        const row = document.createElement("tr");
+
+        const iCell = document.createElement("td");
+        iCell.textContent = i + 1; 
+        row.appendChild(iCell);
+
+        // Mediana
+        const medianaCell = document.createElement("td");
+        medianaCell.textContent = medianaValues[i];
+        row.appendChild(medianaCell);
+
+        // Aritmetična sredina
+        const aritmeticnaSredinaCell = document.createElement("td");
+        aritmeticnaSredinaCell.textContent = averageValues[i];
+        row.appendChild(aritmeticnaSredinaCell);
+
+        // XOR število
+        const xorCell = document.createElement("td");
+        xorCell.textContent = xorValues[i];
+        row.appendChild(xorCell);
+
+        // Binarna mediana
+        const binarnaMedianaCell = document.createElement("td");
+        binarnaMedianaCell.textContent = addLeadingZeros(medianaValues[i], 10);
+        row.appendChild(binarnaMedianaCell);
+        
+        // Binarna aritmetična
+        const binarnaAritmeticnaCell = document.createElement("td");
+        binarnaAritmeticnaCell.textContent = addLeadingZeros(averageValues[i], 10);
+        row.appendChild(binarnaAritmeticnaCell);
+
+        // Binarno XOR
+        const binarnoXorCell = document.createElement("td");
+        binarnoXorCell.textContent = addLeadingZeros(xorValues[i], 10);
+        row.appendChild(binarnoXorCell);
+
+        tbody.appendChild(row);
+      }
+      izrisGrafa(medianaValues, averageValues, xorValues);
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
+}
+
+
+
+function izrisGrafa(medianaValues, averageValues, xorValues) {
+  // Izris grafa
+
+
+  const ctx = document.getElementById('myChart').getContext('2d');
+
+  const labels = medianaValues.map((_, i) => (i + 1).toString()); 
+
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: 'Mediana',
+        data: medianaValues.slice(-25),
+        borderColor: 'red',
+        backgroundColor: 'transparent',
+      },
+      {
+        label: 'Aritmetična sredina',
+        data: averageValues.slice(-25),
+        borderColor: 'blue',
+        backgroundColor: 'transparent',
+      },
+      {
+        label: 'XOR število',
+        data: xorValues.slice(-25),
+        borderColor: 'green',
+        backgroundColor: 'transparent',
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: false,
+      },
+    },
+  };
+  
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: data,
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      
+    },
+  });
+}
+
+const btn = document.getElementById("post");
+
+btn.addEventListener("click", () => {
+  let stevilke = document.getElementById("stevilke").innerHTML;
+  if (stevilke.trim() === "") {
+    alert("Ni podanih števil!");}
+  else {
+  axios.post("http://localhost:5500/api/mediana/calculate", {
+  stevilke: stevilke
+  }, {headers: {"Content-Type": "application/json"} })
+  .then((response) => {
+  console.log(response.data);
+  location.reload(); 
+  })
+  .catch((error) => {
+  console.error(error);
+  });
+}
+});
---
+++ server.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const app = express();
+const PORT = 5500;
+app.use(express.static("public"));
+app.use(express.json()); 
+// Povezava z bazo
+require("dotenv").config();
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  dialect: 'mysql'
+});
+
+async function testPovezava() {
+try {
+  await sequelize.authenticate();
+  console.log('Povezava z bazo podatkov je vzpostavljena.');
+} catch (error) {
+  console.error('Napaka pri povezavi z bazo podatkov: ', error);
+}
+}
+
+const connectionDB = testPovezava()
+
+const mediana_tabelas = require('./models/medianaTabela')(sequelize, Sequelize);
+mediana_tabelas.sync()
+
+app.get("/api/mediana/get", async (req, res) => {
+  try {
+    const result = await mediana_tabelas.findAll({
+      attributes: ['MEDIANA', 'AVERAGE'],
+      order: [['CREATED_AT', 'DESC']],
+      limit: 25
+    });
+
+    const medianaValues = result.map(row => row.MEDIANA);
+    const averageValues = result.map(row => row.AVERAGE);
+
+      res.json({ mediana: medianaValues, average: averageValues});
+  } catch (err) {
+    console.error("Napaka pri izvajanju poizvedbe: ", err);
+    res.status(500).send("Napaka pri izvajanju poizvedbe");
+  }
+});
+
+
+app.post("/api/mediana/calculate", async (req, res) => {
+  console.log(req.body);
+  const stevilke = req.body.stevilke;
+  const timestamp = new Date();
+
+  if (!stevilke) {
+    res.status(400).send("Ni podanih števil");
+    return;
+  } else {
+    const vseStevilke = stevilke.split(',').map(Number);
+    //izračun matematične sredine
+    function getAvg(stevilke) {
+      const average = stevilke.reduce((acc, c) => acc + c, 0);
+      return average / stevilke.length;
+      }
+      const result1 = Math.round(getAvg(vseStevilke));
+      
+      //izračun mediane
+      function median(stevilke) {
+      const sorted = Array.from(stevilke).sort((a, b) => a - b);
+      const middle = Math.floor(sorted.length / 2);
+  
+      if (sorted.length % 2 === 0) {
+      return (sorted[middle - 1] + sorted[middle]) / 2;
+      }
+      return sorted[middle];
+      }
+   
+      const result = Math.round(median(vseStevilke));
+
+      console.log('Mediana:', result);
+      console.log('Sredina:', result1);
+
+      try {
+        const newRecord = await mediana_tabelas.create({
+          CREATED_AT: timestamp,
+          MEDIANA: result,
+          AVERAGE: result1
+        });
+
+      console.log("Podatki uspešno vstavljeni:", JSON.stringify(newRecord));
+
+        res.status(200).send("Podatki uspešno vstavljeni");
+      } catch (err) {
+        console.error("Napaka pri vstavljanju podatkov: ", err);
+        res.status(500).send("Napaka pri vstavljanju podatkov");
+      }
+    }
+});
+
+app.listen(PORT, () => {
+  console.log(`Strežnik je pognan na http://localhost:${PORT}`);
+});
