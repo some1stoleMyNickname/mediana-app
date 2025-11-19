11cf2c8ed3c962f2ba7fed03b78253a9cce49239
Ales Pavli <ales.pavli@gmail.com>
2025-11-19 01:08:08 +0100

    Posodobitve

--- public/index.js
+++ public/index.js
@@ -1,4 +1,5 @@
-const nakljucneStevilke = (min, max, times) => {
+//
+const randNumbers = (min, max, times) => {
   const oneRandomNumber = []
 
   for (let i = 0; i < times; i++) {
@@ -7,21 +8,24 @@ const nakljucneStevilke = (min, max, times) => {
   return oneRandomNumber;
 };
 
+//funkcije ki se sprožijo ob nalaganju okna
 window.onload = function() {
   generateRandomNumbers();
   pStevila();
 };
 
+//generiraj 5 random števil
 document.getElementById("generateRandomNumbers").onclick = function(){
   generateRandomNumbers();
 };
 
 function generateRandomNumbers() {
-  const stevilke = nakljucneStevilke(1, 1000, 5);
+  const stevilke = randNumbers(1, 1000, 5);
   const vseStevilke = document.getElementById("stevilke");
   vseStevilke.innerHTML = stevilke.join(", ");
 };
 
+//izbris enega naključno izbranega števila
 document.getElementById("izbris1").onclick = function(){
   izbris();
 };
@@ -30,11 +34,20 @@ function izbris() {
   const vsaSt = document.getElementById("stevilke");
   const prikSt = vsaSt.innerHTML;
   const stevilke = prikSt.split(", ").map(Number);
+
+//random določi število
+
+  if (stevilke.length <= 1) {
+    alert("Dodajte število/a!");
+  } else {
   const rIzbris = Math.floor(Math.random() * stevilke.length);
   stevilke.splice(rIzbris, 1);
+
   vsaSt.innerHTML = stevilke.join(", ");
+  }
 };
 
+//eno naključno generirano število
 document.getElementById("enoStNakljucno").onclick = function(){
   eno_st();
 }
@@ -42,7 +55,7 @@ document.getElementById("enoStNakljucno").onclick = function(){
 function eno_st() {
   const vseStevilke = document.getElementById("stevilke");
   const pStevila = vseStevilke.innerHTML;
-  const stevilke = nakljucneStevilke(1, 1000, 1);
+  const stevilke = randNumbers(1, 1000, 1);
   const novaStevila = stevilke.join(", ");
   if (pStevila) {
     vseStevilke.innerHTML = pStevila + ", " + novaStevila;
@@ -51,15 +64,21 @@ function eno_st() {
   }
 };
 
+//izbris vseh
 document.getElementById("izbrisVse").onclick = function(){
   izbrisV();
 };
 
 function izbrisV() {
   const abc = document.getElementById("stevilke");
-  abc.innerHTML = [];
+  if (abc.innerHTML == []) {
+    alert("Ni podanih števil!");
+  } else {
+    abc.innerHTML = [];
+  };
 };
 
+//dodaj število
 function formSubmit() {
   const form = document.forms["selectNumberSlider"];
   const inputValue = form["amountInput"].value;
@@ -90,6 +109,7 @@ function calculateXOR(stevilke) {
   return xorValue;
 }
 
+//podatki za tabelo
 function pStevila() {
   axios.get("http://localhost:5500/api/mediana/get")
     .then(function(response) {
@@ -100,6 +120,7 @@ function pStevila() {
 
       const tbody = document.querySelector("#TabelaSt tbody");
 
+      // Izračun XOR med stevilo in avgValue na vsakem mestu
       for (let i = 0; i < medianaValues.length && i < averageValues.length; i++) {
         const xorValue = calculateXOR([medianaValues[i], averageValues[i]]);
         xorValues.push(xorValue);
@@ -119,26 +140,32 @@ function pStevila() {
         iCell.textContent = i + 1; 
         row.appendChild(iCell);
 
+        // Mediana
         const medianaCell = document.createElement("td");
         medianaCell.textContent = medianaValues[i];
         row.appendChild(medianaCell);
 
+        // Aritmetična sredina
         const aritmeticnaSredinaCell = document.createElement("td");
         aritmeticnaSredinaCell.textContent = averageValues[i];
         row.appendChild(aritmeticnaSredinaCell);
 
+        // XOR število
         const xorCell = document.createElement("td");
         xorCell.textContent = xorValues[i];
         row.appendChild(xorCell);
 
+        // Binarna mediana
         const binarnaMedianaCell = document.createElement("td");
         binarnaMedianaCell.textContent = addLeadingZeros(medianaValues[i], 10);
         row.appendChild(binarnaMedianaCell);
         
+        // Binarna aritmetična
         const binarnaAritmeticnaCell = document.createElement("td");
         binarnaAritmeticnaCell.textContent = addLeadingZeros(averageValues[i], 10);
         row.appendChild(binarnaAritmeticnaCell);
 
+        // Binarno XOR
         const binarnoXorCell = document.createElement("td");
         binarnoXorCell.textContent = addLeadingZeros(xorValues[i], 10);
         row.appendChild(binarnoXorCell);
@@ -152,27 +179,34 @@ function pStevila() {
     });
 }
 
+
+
 function izrisGrafa(medianaValues, averageValues, xorValues) {
+  // Izris grafa
+
+
   const ctx = document.getElementById('myChart').getContext('2d');
+
   const labels = medianaValues.map((_, i) => (i + 1).toString()); 
+
   const data = {
     labels: labels,
     datasets: [
       {
         label: 'Mediana',
-        data: medianaValues,
+        data: medianaValues.slice(-25),
         borderColor: 'red',
         backgroundColor: 'transparent',
       },
       {
         label: 'Aritmetična sredina',
-        data: averageValues,
+        data: averageValues.slice(-25),
         borderColor: 'blue',
         backgroundColor: 'transparent',
       },
       {
         label: 'XOR število',
-        data: xorValues,
+        data: xorValues.slice(-25),
         borderColor: 'green',
         backgroundColor: 'transparent',
       },
@@ -199,8 +233,10 @@ function izrisGrafa(medianaValues, averageValues, xorValues) {
     type: 'line',
     data: data,
     options: {
-      responsive: false
-      },
+      responsive: true,
+      maintainAspectRatio: false,
+      
+    },
   });
 }
 
